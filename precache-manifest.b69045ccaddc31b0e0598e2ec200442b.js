cd7aaa71978d106b0b69c198d4b3e699699e35c7
anony <anony@anony>
2022-05-05 00:18:30 +0800

    deploy

---
+++ precache-manifest.b69045ccaddc31b0e0598e2ec200442b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd813fec090f8ed7fd29fa197f178167",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aae8c449fecae8dda045",
+    "url": "/static/css/2.1145c33d.chunk.css"
+  },
+  {
+    "revision": "606941678dc3d1917507",
+    "url": "/static/css/main.fce97c22.chunk.css"
+  },
+  {
+    "revision": "aae8c449fecae8dda045",
+    "url": "/static/js/2.fc1ee60e.chunk.js"
+  },
+  {
+    "revision": "2a96075e8bba85627a679e14b48bdc92",
+    "url": "/static/js/2.fc1ee60e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "606941678dc3d1917507",
+    "url": "/static/js/main.c62e5616.chunk.js"
+  },
+  {
+    "revision": "9badd24699e8345b5d66",
+    "url": "/static/js/runtime-main.fefedfa5.js"
+  }
+]);
