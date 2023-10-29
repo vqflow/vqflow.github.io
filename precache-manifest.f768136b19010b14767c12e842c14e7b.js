5eec4fc6d1895a1c93ce980eab1c8b40b49aa59d
anony <anony@anony>
2023-10-29 22:03:57 +0800

    first commit

---
+++ precache-manifest.f768136b19010b14767c12e842c14e7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f2df95920ef7ff2b009a6a05b3c6754",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aae8c449fecae8dda045",
+    "url": "/static/css/2.1145c33d.chunk.css"
+  },
+  {
+    "revision": "1aebd2221ad8f50feed5",
+    "url": "/static/css/main.fce97c22.chunk.css"
+  },
+  {
+    "revision": "aae8c449fecae8dda045",
+    "url": "/static/js/2.fc1ee60e.chunk.js"
+  },
+  {
+    "revision": "2a96075e8bba85627a679e14b48bdc92",
+    "url": "/static/js/2.fc1ee60e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aebd2221ad8f50feed5",
+    "url": "/static/js/main.7eef5927.chunk.js"
+  },
+  {
+    "revision": "9badd24699e8345b5d66",
+    "url": "/static/js/runtime-main.fefedfa5.js"
+  }
+]);
